72280f39906c360a80f60a570679aeae63e101c3
smashthewindow <smashthewindow@3ae7135b-1e0b-4db2-b2b9-6a841f3ea2cd>
2013-02-27 15:37:41 +0000

    Changed credits page.
    git-svn-id: https://subversion.assembla.com/svn/rebounce@112 3ae7135b-1e0b-4db2-b2b9-6a841f3ea2cd

--- trunk/GameSrc/GUI/CreditsPage.cpp
+++ trunk/GameSrc/GUI/CreditsPage.cpp
@@ -21,31 +21,10 @@ GWEN_CONTROL_CONSTRUCTOR(CreditsPageControl)
 	std::string creditsText =
 		"	\"moon.chase.star\""
 		"\n	"
-		"\n	"
-		"\n	"
 		"\n	developers:"
 		"\n	lead development / programming - Il Jae Lee (j.lee)"
 		"\n	level design - Clayton Kramp (genki)"
-		"\n	"
-		"\n	"
-		"\n	"
-		"\n	special thanks to:"
-		"\n	whoever wrote that poem about chasing stars on Birmingham Starbucks"
-		"\n	Joe St. Germain, for the beta testing and breaking builds of the game"
-		"\n	Laurent Gomila, for making this awesome rendering library SFML"
-		"\n	Erin Catto, the genius who wrote Box2D"
-		"\n	Kevin MacLeod, for releasing his works under Creative Commons license (incompetech.com)"
-		"\n	Joohee Jin, for listening to my complaints during the development"
-		"\n	... and YOU, for being part of this creative communication"
-		"\n	"
-		"\n	"
-		"\n	"
-		"\n	remembering Caleb, forever shining in our hearts"
-		"\n	"
-		"\n	"
-		"\n	"
-		"\n	thanks all for playing, it's been a fun ride."
-		"\n	- 2013.02.25, j & genki from Cranbrook";
+		"\n	Kevin MacLeod, for releasing his works under Creative Commons license (incompetech.com)";
 
 	m_creditsFont = new Gwen::Font;
 	m_creditsFont->facename = L"Resource/Fonts/Stroke-Bold.otf";
